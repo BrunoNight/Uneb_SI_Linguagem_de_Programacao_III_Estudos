6cd8c5db81b751d5221d997c932704aae50353c0
BrunoNight <brunobrrt3680@gmail.com>
2026-09-03 12:41:39 -0300

    Add Thread_Basic_2 class for thread name printing
    This class demonstrates how to create and print the names of the current thread and a newly created thread.

--- Caderno_Aula_19x08x2026.java
+++ Caderno_Aula_19x08x2026.java
@@ -14,3 +14,27 @@ public class Thread_Basic { // Classe para básica obtenção de thread atual, n
 
 // === === === === === === === === === === === === === === === === === === //
 
+// -> Classe para impressão dos nomes da thread atual e de uma thread criada do zero
+
+// Classe para impressão dos nomes da thread atual
+// e de uma thread criada do zero
+public class Thread_Basic_2 { 
+    public static void main(String[] args) {
+        // Variável thread que obtém a Thread atual 'main'
+        Thread tMain = Thread.currentThread();
+        
+        // Variável thread para criar uma nova Thread, sendo 'Thread-0'
+        // ao não ser atribuído um nome à ela
+        Thread tCriada1 = new Thread();
+        
+        // Variável thread para criar uma nova Thread, sendo 'Thread_Bala'
+        // ao atribuir um nome à ela
+        Thread tCriada2 = new Thread("Thread_Bala");
+        
+        // Impressão dos nomes das threads
+        System.out.println("Thread atual: " + tMain.getName()
+        + "\n" + "Thread Criada 1: " + tCriada1.getName() + "\n" 
+        + "Thread Criada 2: " + tCriada2.getName()
+        );
+    }
+}
