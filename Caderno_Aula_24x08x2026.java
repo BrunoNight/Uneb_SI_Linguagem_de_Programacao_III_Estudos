b2e9575ebe1f4f9853183d15938b1bd7fdbcce98
BrunoNight <brunobrrt3680@gmail.com>
2026-09-07 15:31:24 -0300

    Add synchronized withdrawal method to ContaBancaria
    Implemented synchronized withdrawal method in ContaBancaria class to handle concurrent access safely.

--- Caderno_Aula_24x08x2026.java
+++ Caderno_Aula_24x08x2026.java
@@ -39,3 +39,61 @@ public class MeuRunnable implements Runnable {
         + Thread.currentThread().getName());
     }
 }
+
+// === === === === === === === === === === === === === === === === === === //
+
+// -> Classe para execução de saques em conta bancária de forma sincronizada
+
+public class ContaBancaria {
+    private int saldo = 1000;
+    
+    public int getSaldo() {
+        return this.saldo;
+    }
+    
+    public synchronized void sacar(int valor) {
+        String nomeThread = Thread.currentThread().getName();
+        
+        if(saldo < valor) {
+            System.out.println("Saldo insuficiente " + nomeThread + "!");
+            return;
+        } else if(saldo == 0) {
+            System.out.println("Saldo zerado " + nomeThread + "!");
+            return;
+        }
+        
+        this.saldo -= valor;
+        System.out.println("Saque de " + valor + " feito com sucesso!");
+    }
+    
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria();
+        
+        MeuRunnable rn = new MeuRunnable(conta);
+        
+        Thread t1 = new Thread(rn);
+        Thread t2 = new Thread(rn);
+        t1.start();
+        t2.start();
+    }
+}
+
+// -> Classe MeuRunnable para cada thread executar o saque na conta bancária
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MeuRunnable implements Runnable {
+    private ContaBancaria conta;
+    
+    public MeuRunnable(ContaBancaria c) {
+        this.conta = c;
+    }
+    
+    @Override
+    public void run() {
+        int valorAleatorio = ThreadLocalRandom.current().nextInt(50, 1000);
+        conta.sacar(valorAleatorio);
+    }
+}
+
+
